090c108734942dd9e95c5c45b1795d6f2acf95e4
dwirifki10 <dwirifkinovianto10@gmail.com>
2022-06-13 12:34:42 +0700

    add register page

--- gym-web/pages/admin/RegisterPage.vue
+++ gym-web/pages/admin/RegisterPage.vue
@@ -1,35 +1,14 @@
 <template>
-  <div class="col-lg-8 center">
-    <div class="card shadow-sm">
-      <div class="card-body">
-        <img src="@/assets/img/logo-transparan.png" alt="Logo" width="350" />
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
-          </div>
-        </form>
-      </div>
-    </div>
+  <div>
+    <RegisterView />
   </div>
 </template>
 
 <script>
+import RegisterView from '@/components/Dashboard/RegisterView.vue'
 export default {
-  data() {
-    return {
-      name: 'Dwi Rifki Novianto',
-    }
-  },
+  name: 'RegisterPage',
+  components: { RegisterView },
 }
 </script>
 <style scoped>
