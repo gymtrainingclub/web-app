18850328894aa84880ec210caf810de817d5f66e
dwirifki10 <dwirifkinovianto10@gmail.com>
2022-07-08 22:03:58 +0700

    add index page

---
+++ gym-web/components/User/Content/FooterView.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="col-lg-12 bg-custom text-white">
+		<div class="row g-0 p-5">
+			<div class="col-lg-3 mb-3">
+				<h5 class="px-3">Company</h5>
+				<ul class="list-unstyled pt-2 px-3">
+					<li class="pb-2">About Us</li>
+					<li class="pb-2">Blog</li>
+					<li class="pb-2">Careers</li>
+					<li class="pb-2">Contact Us</li>
+				</ul>
+			</div>
+			<div class="col-lg-3 mb-3">
+				<h5 class="px-3">Support</h5>
+				<ul class="list-unstyled pt-2 px-3">
+					<li class="pb-2">Help Center</li>
+					<li class="pb-2">Safety Center</li>
+					<li class="pb-2">Community Guidelines</li>
+				</ul>
+			</div>
+			<div class="col-lg-3 mb-3">
+				<h5 class="px-3">Legal</h5>
+				<ul class="list-unstyled pt-2 px-3">
+					<li class="pb-2">Cookies Policy</li>
+					<li class="pb-2">Privacy Policy</li>
+					<li class="pb-2">Terms of Service</li>
+					<li class="pb-2">Low Enforcement</li>
+				</ul>
+			</div>
+			<div class="col-lg-3 mb-3">
+				<h5 class="px-3">Install App</h5>
+				<ul class="list-unstyled pt-2">
+					<li>
+						<img src="@/assets/img/installer.jpg" width="150" />
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'FooterView',
+}
+</script>
+
+<style scoped>
+.bg-custom {
+	background: #377a8a;
+}
+</style>
---
+++ gym-web/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<NavbarView />
+		<h1>Content</h1>
+		<FooterView />
+	</div>
+</template>
+
+<script>
+import NavbarView from '@/components/User/Content/NavbarView.vue'
+import FooterView from '@/components/User/Content/FooterView.vue'
+
+export default {
+	name: 'LandingPage',
+	components: { NavbarView, FooterView },
+}
+</script>
