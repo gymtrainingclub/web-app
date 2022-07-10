b858d2bdef10a12b55cf28a73b4067dc4f2262ca
kristinargg <rggkristina26@gmail.com>
2022-07-11 00:09:31 +0700

    add home page user

--- gym-web/components/User/Content/NavbarView.vue
+++ gym-web/components/User/Content/NavbarView.vue
@@ -1,63 +1,63 @@
 <template>
-	<nav class="navbar navbar-expand-lg bg-custom">
-		<NuxtLink class="navbar-brand" to="/">
-			<img src="@/assets/img/logo.png" width="65" />
-		</NuxtLink>
-		<button
-			class="navbar-toggler shadow-sm"
-			type="button"
-			data-toggle="collapse"
-			data-target="#navbarSupportedContent"
-			@click.stop="toggleNavbar()"
-		>
-			<b-icon icon="chevron-bar-down" class="text-white h3"></b-icon>
-		</button>
-		<div
-			class="collapse navbar-collapse justify-content-end"
-			id="navbarSupportedContent"
-			v-bind:class="{ show: show }"
-		>
-			<ul class="navbar-nav mr-5">
-				<li class="nav-item mr-5 mb-2">
-					<NuxtLink to="/auth/user/register" class="btn btn-white text-white">
-						Sign Up
-					</NuxtLink>
-				</li>
-				<li class="nav-item mb-2">
-					<NuxtLink
-						to="/auth/user/login"
-						class="btn btn-custom rounded-pill px-5"
-					>
-						Login
-					</NuxtLink>
-				</li>
-			</ul>
-		</div>
-	</nav>
+  <nav class="navbar navbar-expand-lg bg-custom">
+    <NuxtLink class="navbar-brand" to="/">
+      <img src="@/assets/img/logo.png" width="65" />
+    </NuxtLink>
+    <button
+      class="navbar-toggler shadow-sm"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      @click.stop="toggleNavbar()"
+    >
+      <b-icon icon="chevron-bar-down" class="text-white h3"></b-icon>
+    </button>
+    <div
+      class="collapse navbar-collapse justify-content-end"
+      id="navbarSupportedContent"
+      v-bind:class="{ show: show }"
+    >
+      <ul class="navbar-nav mr-5">
+        <li class="nav-item mr-5 mb-2">
+          <NuxtLink to="/auth/user/register" class="btn btn-white text-white">
+            Sign Up
+          </NuxtLink>
+        </li>
+        <li class="nav-item mb-2">
+          <NuxtLink
+            to="/auth/user/login"
+            class="btn btn-custom rounded-pill px-5"
+          >
+            Login
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-	name: 'NavbarView',
-	data() {
-		return {
-			show: false,
-		}
-	},
-	methods: {
-		toggleNavbar() {
-			this.show = !this.show
-		},
-	},
+  name: 'NavbarView',
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    toggleNavbar() {
+      this.show = !this.show
+    },
+  },
 }
 </script>
 
 <style scoped>
 .btn-custom {
-	background: #006784;
-	color: white;
+  background: #006784;
+  color: white;
 }
 .bg-custom {
-	background: #0c303d;
+  background: #0c303d;
 }
 </style>
---
+++ gym-web/pages/User/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HomeView />
+  </div>
+</template>
+<script>
+import HomeView from '@/components/User/HomeView.vue'
+export default {
+  name: 'HomePage',
+  components: {
+    HomeView,
+  },
+}
+</script>
